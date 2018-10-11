6352fbe4fc55c47aeb5a7e43c8b3d48d8fcb8fef
jason <jason.addie@gmail.com>
2018-10-11 16:04:47 +0400

    added article page links/logic

--- src/pages/Article/index.jsx
+++ src/pages/Article/index.jsx
@@ -1,11 +1,38 @@
 import React, { Component } from 'react'
+import { Redirect } from 'react-router-dom'
+
+import articles from './data.json'
 
 import './style.css'
 
 class Article extends Component {
+  constructor(props) {
+    super(props)
+
+    console.log(props)
+  }
+
   render () {
+    console.log(this.props)
+
+    const splitPath = this.props.match.params.article_id.split('/')
+    const article_id = splitPath.slice(-1)[0]
+    const article = article_id ? articles[article_id] : null
+
     return (
-      <div>article</div>
+      <div>
+        {
+          article ? (
+              <div>
+                <h1>{article.title}</h1>
+
+                <div>{article.content}</div>
+              </div>
+          ) : (
+            <Redirect to='/' />
+          )
+        }
+      </div>
     )
   }
 }
--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,11 +1,27 @@
 import React, { Component } from 'react'
+import { Link } from 'react-router-dom'
+
+import articles from '../Article/data.json'
 
 import './style.css'
 
 class Home extends Component {
   render () {
     return (
-      <div>home</div>
+      <div>
+        <p>home</p>
+
+        <ul className='articles'>
+          {
+            Object.keys(articles).map((key) => {
+              return <li key={key}>
+                <div className='article-title'><Link to={`/article/${key}`}>{articles[key].title}</Link></div>
+                <div className='article-summary'>{articles[key].summary}</div>
+              </li>
+            })
+          }
+        </ul>
+      </div>
     )
   }
 }
