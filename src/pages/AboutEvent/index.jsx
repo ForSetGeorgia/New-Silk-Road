44a7515f836ff3e611993e73147c33793d54076c
jason <jason.addie@gmail.com>
2018-10-12 00:03:07 +0400

    added carousel to about event

--- src/pages/AboutEvent/index.jsx
+++ src/pages/AboutEvent/index.jsx
@@ -1,4 +1,8 @@
 import React, { Component } from 'react'
+import "react-responsive-carousel/lib/styles/carousel.min.css"
+import { Carousel } from 'react-responsive-carousel'
+
+import images from './data.json'
 
 import './style.css'
 
@@ -11,6 +15,26 @@ class AboutEvent extends Component {
         <p>Lorem ipsum dolor sit amet consectetur adipiscing elit nibh tristique, morbi inceptos tempor a elementum fusce placerat facilisis ut eget, ante quis quam cubilia mauris leo enim suspendisse. Conubia sociis mi hendrerit metus cubilia risus nulla, porta nostra sociosqu libero elementum ligula lacus, bibendum malesuada curae pharetra varius erat. Lobortis libero ut cubilia pretium cras eget senectus euismod, parturient tempus convallis magna facilisis dictum accumsan, duis quis praesent nascetur eleifend ridiculus phasellus.</p>
         <p>Commodo tellus felis condimentum a vestibulum lectus egestas feugiat nascetur, ac neque cursus diam duis justo facilisis ullamcorper. Facilisis nisl litora neque penatibus curae at semper nostra pellentesque, varius rhoncus justo tempus eleifend mattis pharetra aliquet, hendrerit convallis tincidunt habitant aliquam primis magnis eget. Torquent ullamcorper risus orci aenean donec scelerisque dis, enim condimentum tempor lacus vel ad taciti tincidunt, in morbi per facilisi commodo class. Litora quis condimentum vivamus nec accumsan tincidunt pharetra sem, proin ultricies dignissim lobortis suscipit eu molestie, volutpat euismod interdum bibendum egestas commodo aenean.</p>
         <p>Consequat imperdiet ligula orci pharetra aenean sed litora aliquam elementum dis, maecenas accumsan in primis felis vitae platea condimentum et. Vestibulum tempus euismod nostra praesent morbi sollicitudin sagittis convallis vivamus ante magna porta, ut tempor venenatis proin nam facilisi lacus fames accumsan iaculis libero, nunc fermentum vehicula per non ridiculus mus sodales primis mattis mollis. Cum nulla lacus phasellus egestas erat quis posuere, malesuada arcu sed non dapibus netus, tempus felis torquent nullam class gravida.</p>
+
+        <Carousel centerMode useKeyboardArrows
+                  centerSlidePercentage={75}
+                  ref={(ref) => (this.carousel = ref)}
+                  showThumbs={false} showStatus={false}
+                  infiniteLoop={true} selectedItem={1}>
+          {
+            images.map((image, key) => {
+              return (
+                <div key={key}>
+                  <img src={require(`../../images/${image.image}`)}/>
+                </div>
+              )
+            })
+          }
+
+
+
+        </Carousel>
+
       </div>
     )
   }
